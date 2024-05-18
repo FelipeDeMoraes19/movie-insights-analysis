4bbb3b8d155122433b0643e2245390739653eb74
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-18 12:02:52 -0300

    Implement data cleaning function

---
+++ src/data_cleaning.py
@@ -0,0 +1,5 @@
+def clean_data(df):
+    df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+    df = df.dropna(subset=['title', 'genres', 'release_date', 'budget', 'revenue', 'vote_average'])
+    df = df[(df['budget'] > 0) & (df['revenue'] > 0)]
+    return df
